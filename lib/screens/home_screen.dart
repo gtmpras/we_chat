501ba260d9e9f0e94c9e059fb2eb3576a2252624
Prasoon Gautam <109397711+gtmpras@users.noreply.github.com>
2024-07-21 13:41:02 +0545

    User Search Feature

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,20 +12,23 @@ import '../models/chat_user_models.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-  
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-_signOut()async{
+
+_signOut() async {
   await APIs.auth.signOut();
   await GoogleSignIn().signOut();
 }
 
-
 class _HomeScreenState extends State<HomeScreen> {
+  //for storing all users
+  List<ChatUserModel> _list = [];
+  List<ChatUserModel> _searchList = [];
 
-  List<ChatUserModel> list = [];
+  //for storing search status
+  bool _isSearching = false;
 
   @override
   void initState() {
@@ -33,73 +36,107 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     APIs.getSelfInfo();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: Icon(CupertinoIcons.home),
-        title: Text(title),
+        title: _isSearching? TextField(
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Name, Email,...',
+          ),
+          autofocus: true,
+          style: TextStyle(fontSize:16,letterSpacing: 0.5),
+          onChanged: (val){
+            //search logic
+          _searchList.clear();
+          for (var i in _list){
+            if(i.name.contains(val.toLowerCase()) || i.email.contains(val.toLowerCase())){
+              _searchList.add(i);
+            }
+            setState(() {
+              _searchList;
+            });
+          }
+          },
+        ):Text(title),
         actions: [
-          IconButton(onPressed: (){}, 
-          icon: Icon(Icons.search)),
-          IconButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (_)=>ProfileScreen(user: APIs.me)));
-          }, 
-          icon: Icon(Icons.more_vert)),
-          IconButton(onPressed: (){
-            _signOut();
-            Navigator.push(context,
-            MaterialPageRoute(builder: (_)=>LoginScreen()));
-            print('Logged Out Successfully');
-          }, 
-          icon: Icon(Icons.run_circle_outlined)),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  _isSearching = !_isSearching;
+                });
+              },
+              icon: Icon(_isSearching
+                  ? CupertinoIcons.clear_circled_solid
+                  : Icons.search)),
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => ProfileScreen(user: APIs.me)));
+              },
+              icon: Icon(Icons.more_vert)),
+          IconButton(
+              onPressed: () {
+                _signOut();
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => LoginScreen()));
+                print('Logged Out Successfully');
+              },
+              icon: Icon(Icons.run_circle_outlined)),
         ],
       ),
 
       floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom:10.0),
-        child: FloatingActionButton(onPressed: (){},
-        child: Icon(Icons.add_comment_rounded),),
+        padding: const EdgeInsets.only(bottom: 10.0),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add_comment_rounded),
+        ),
       ),
 //fetching data from cloud firestore firebase.
       body: StreamBuilder(
-        stream: APIs.getAllUser(),
-        builder: (context, snapshot){
-          switch(snapshot.connectionState){
-            //if some time takes exectue this
-            case ConnectionState.waiting:
-            case ConnectionState.none:
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-            //if data is loaded then execute this
-          case ConnectionState.active:
-          case ConnectionState.done:
+          stream: APIs.getAllUser(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              //if some time takes exectue this
+              case ConnectionState.waiting:
+              case ConnectionState.none:
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              //if data is loaded then execute this
+              case ConnectionState.active:
+              case ConnectionState.done:
+                final data = snapshot.data?.docs;
+                _list = data
+                        ?.map((e) => ChatUserModel.fromJson(e.data()))
+                        .toList() ??
+                    [];
 
-         
-            final data = snapshot.data?.docs;
-            list = data?.map((e)=> ChatUserModel.fromJson(e.data())).toList()??[];
-          
-          if(list.isNotEmpty){
-            
-          return Container(
-            child: ListView.builder(
-            padding: EdgeInsets.only(top: mq.height * .01),
-            itemCount: list.length,
-            physics: BouncingScrollPhysics(),
-            itemBuilder: (context,index){
-            return ChatUserCard(user: list[index]);
-            
-                    }),
-          );
-          }else{
-            return 
-            Center(child: Text(emptycolud_store,style:TextStyle(fontSize: 20),));
-          }
-          }
-        }   
-      ),
+                if (_list.isNotEmpty) {
+                  return Container(
+                    child: ListView.builder(
+                        padding: EdgeInsets.only(top: mq.height * .01),
+                        itemCount:_isSearching? _searchList.length: _list.length,
+                        physics: BouncingScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return ChatUserCard(user:_isSearching? _searchList[index]: _list[index]);
+                        }),
+                  );
+                } else {
+                  return Center(
+                      child: Text(
+                    emptycolud_store,
+                    style: TextStyle(fontSize: 20),
+                  ));
+                }
+            }
+          }),
     );
   }
 }

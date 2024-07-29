9971ccb36244fb888b404327da2648be24f9ce52
Prasoon Gautam <109397711+gtmpras@users.noreply.github.com>
2024-07-29 13:59:09 +0545

    sent message time

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'dart:developer';
-import 'dart:ffi';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
--- lib/widgets/chat_user_card.dart
+++ lib/widgets/chat_user_card.dart
@@ -1,9 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:we_chat/api/api.dart';
 import 'package:we_chat/main.dart';
 import 'package:we_chat/models/chat_user_models.dart';
+import 'package:we_chat/models/message_model.dart';
 import 'package:we_chat/screens/chat_screen.dart';
+import 'package:we_chat/utils/my_date_util.dart';
+import 'package:we_chat/widgets/message_card.dart';
 
 class ChatUserCard extends StatefulWidget {
   final ChatUserModel user;
@@ -14,6 +18,11 @@ class ChatUserCard extends StatefulWidget {
 }
 
 class _ChatUserCardState extends State<ChatUserCard> {
+
+  //last message info (if null -> no message)
+  //checking message from MessageModel file
+  MessageModel? _message;
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -21,10 +30,18 @@ class _ChatUserCardState extends State<ChatUserCard> {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       elevation: 2,
       child: InkWell(
-        child: ListTile(
+        child: StreamBuilder(stream: APIs.getLastMessage(widget.user), builder: (context, snapshot){
+
+          final data = snapshot.data?.docs;
+          final _list = data?.map((e)=> MessageModel.fromJson(e.data())).toList()??[];
+          if(_list.isNotEmpty){
+            _message = _list[0];
+          }
+        return  ListTile(
           onTap: (){
             Navigator.push(context, MaterialPageRoute(builder: (_)=> ChatScreen(user: widget.user)));
           },
+          //user profile picture
           leading: ClipRRect(
             borderRadius: BorderRadius.circular(mq.height * .3),
             child: CachedNetworkImage(
@@ -36,14 +53,21 @@ class _ChatUserCardState extends State<ChatUserCard> {
               ),
           ),
           title: Text(widget.user.name),
-          subtitle: Text(widget.user.about,maxLines: 1,),
-          trailing: Container(
+          subtitle: Text(_message !=null? _message!.msg : widget.user.about,maxLines: 1,),
+          //last message time
+          trailing: _message==null 
+          ? null
+          :_message!.read.isEmpty && _message!.fromId != APIs.user.uid? Container(
             width: 10,height: 10,
-            decoration: BoxDecoration(color: Colors.green,borderRadius: BorderRadius.circular(50)),
-             
+            decoration: BoxDecoration(color: Colors.green,borderRadius: BorderRadius.circular(50)),    
+          )
+          :Text(
+           MyDateUtil.getLastMessageTime(context: context, time: _message!.sent),
+            style: TextStyle(color: Colors.black),
           ),
     
-        ),
+        );
+        })
       ),
     );
   }
